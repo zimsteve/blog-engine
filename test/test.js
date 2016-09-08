817030bfc9af53f6e8abbb75bd2ab350c4cc9035
Steve Sharp <email": null>
2016-09-08 13:57:14 +1200

    TDD working

--- test/test.js
+++ test/test.js
@@ -1,14 +1,23 @@
 var test = require('tape')
-
-
+var db = require('../db/db')
 
 test('get all posts', function(t){
 
   //arrange
 
+var expectedPostObj = {
+  posts: [
+    {"id": 1, "name": "first-post", "image": "http://www.knoxroad.com/wp-content/uploads/2009/05/cute-kitten.jpg"}
+  ]
+}
   //act
+  db.getAll('posts', function (err, postsObj) {
 
   //assert
+  t.deepEqual(postsObj, expectedPostObj, 'get function should return all posts')
+  t.end()
+  })
+
 
 
 })
